1e5a822cfcfc48656c79791c59c5e4abb23bcfc6
JasonOrosco <jason.orosco@pinksummit.com>
2026-01-08 18:49:28 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vighter.condor',
+  appName: 'condor-app',
+  webDir: 'www'
+};
+
+export default config;
